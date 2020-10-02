52599b266563d036dc9d8b1addd6025ef28a7faa
Xerousso <email.squishling@gmail.com>
2020-10-02 14:52:31 +0400

    Spell execution in casting item

--- src/main/java/club/mcmodding/salem/SpellCastingItem.java
+++ src/main/java/club/mcmodding/salem/SpellCastingItem.java
@@ -1,18 +1,22 @@
 package club.mcmodding.salem;
 
 import club.mcmodding.salem.spells.Spell;
+import club.mcmodding.salem.spells.SpellRegistry;
 import club.mcmodding.salem.util.Util;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.hit.HitResult;
 
 /**
  * A spell casting item is any item that has the ability to hold and execute {@link Spell}s.  They have a radial menu to select
  * {@link Spell}s, a certain amount of 'energy', and a spell capacity and energy capacity.  Custom spell casting items can contain
  * custom logic to determine how much power to give each {@link Spell} based on the stats given to the particular spell casting item.
  */
-public class SpellCastingItem extends Item {
+public abstract class SpellCastingItem extends Item {
 
     private int capacity = 8;
     private float energyCapacity = 1000f;
@@ -99,6 +103,33 @@ public class SpellCastingItem extends Item {
         if (!simulate) tag.putFloat("energy", Util.clamp(currentEnergy + energy, 0f, energyCapacity));
 
         return Math.max(energyCapacity - currentEnergy - energy, 0f);
+    }
+
+    /** If the {@link Spell} is usable by this particular spell casting item. */
+    public boolean canUse(Spell spell) {
+        return true;
+    }
+
+    /**  Calculate the amount of power given to the {@link Spell}.  Minimum is {@code 1f}, normal is {@code} 5f, maximum is {@code 9f}.*/
+    protected abstract float getPower(ItemStack stack, Spell spell);
+
+    /**
+     * Executes the selected {@link Spell}'s {@code execute} method with {@link SpellCastingItem#getPower(ItemStack, Spell)} power.
+     * @param entity The entity who casted the {@link Spell}.
+     * @param raycastHitResult The {@link HitResult} of a raycast from the {@code entity}.
+     * @param spellCastingItem The {@link ItemStack} used to cast this {@link Spell}.
+     */
+    protected final void execute(LivingEntity entity, HitResult raycastHitResult, ItemStack spellCastingItem) {
+        CompoundTag tag = getOrCreateTag(spellCastingItem);
+        ListTag listTag = (ListTag) tag.get("spell_list");
+
+        Spell spell = null;
+        if (listTag != null) spell = SpellRegistry.SPELL_TYPE.get(new Identifier(listTag.getString(tag.getInt("selected_spell_index"))));
+
+        if (spell != null && canUse(spell) && spell.execute(entity, raycastHitResult, spellCastingItem, Util.clamp(getPower(spellCastingItem, spell), 1f, 9f))) {
+            removeEnergy(spellCastingItem, spell.simulateEnergyUsage(entity, raycastHitResult, spellCastingItem,
+                    Util.clamp(getPower(spellCastingItem, spell), 1f, 9f), spell.getBaselineEnergyUse()));
+        }
     }
 
     /** Get the {@code stack}'s compound nbt tag if present, otherwise create and fill one. */
